147354ada5f260cd8fc59fa5a08a8dd213f03aa0
Nikita Patil <nikita.patil@entercoms.com>
2021-06-22 11:27:50 +0530

    Bill Split App

---
+++ test.js
@@ -0,0 +1,8 @@
+var name1 = prompt("Enter your Name")
+document.write("Hello ", name1, "</br>")
+
+
+var total_amount = prompt("Enter the total Bill amount ")
+var people = prompt("Number of people ")
+
+document.write("Each Person will pay Rs: ", total_amount/people, "</br>")
